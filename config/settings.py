c3971eab21492865b4020ccb40ed7b510a6be0c8
minsuk1003 <kms1003go@khu.ac.kr>
2024-04-04 19:23:36 +0900

    04.04

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'bootstrap4',
     'corsheaders',
     'news_guru.apps.NewsGuruConfig',
-    'django-environ',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
